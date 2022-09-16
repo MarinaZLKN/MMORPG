ef8f3d3b6b4b157c79ff38ce9b1d1c8b72b87e48
Marina <marina.zilkina@gmail.com>
2022-09-16 10:42:59 +0300

    Повключена регистрация via Google, но не работает.

--- TheBoard/TheBoard/urls.py
+++ TheBoard/TheBoard/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('GameBoard.urls')),
     path('sign/', include('sign.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
